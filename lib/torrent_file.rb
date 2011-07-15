22fe6898bba9699d043d51294f4c8ecd585d240c
Mikhail Slyusarev <slyusarevmikhail@gmail.com>
2011-07-14 22:26:42 -0700

    documentation

--- lib/torrent_file.rb
+++ lib/torrent_file.rb
@@ -2,20 +2,26 @@
 
 require_relative 'bencode.rb'
 
+# Class for easily handling .torrent files.
+
 class TorrentFile
+  # Create a TorrentFile object with/without the dictionary.
   def initialize contents = {}
     @contents = contents
     @bencoded_contents = @contents.bencode
   end
   
+  # Change contents of TorrentFile.
   def create contents = {}
     @contents = contents
   end
   
+  # Get the Hash representation of the TorrentFile's dictionary.
   def to_h
     @contents
   end
   
+  # Create from a file (object or filename).
   def self.open f
     f = File.open f, 'rb' unless f.class == File
     @bencoded_contents = f.read.strip
@@ -23,6 +29,7 @@ class TorrentFile
     TorrentFile.new @contents
   end
   
+  # Save TorrentFile to file with name 'f'.
   def save f
     File.open(f, 'wb') do |file|
       file.write @bencoded_contents
--- test/test_bencode.rb
+++ test/test_bencode.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
-require '../lib/bencode.rb'
+require './lib/bencode.rb'
 
 class TestString < Test::Unit::TestCase
   def test_bencode

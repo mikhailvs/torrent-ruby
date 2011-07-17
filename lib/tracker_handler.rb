7dacab3f4f574dc72a09169d15ed60c09c1d663e
Mikhail Slyusarev <slyusarevmikhail@gmail.com>
2011-07-16 21:31:50 -0700

    lotsa work

--- lib/tracker_handler.rb
+++ lib/tracker_handler.rb
@@ -5,30 +5,48 @@ require 'digest/sha1'
 require 'uri'
 require 'timeout'
 
-require_relative 'torrent_file.rb'
+# Class for handling things related to trackers. It serves as a common place to perform
+# actions with all the trackers defined in a .torrent metainfo file's dictionary.
 
 class TrackerHandler
   attr_reader :connected_trackers, :disconnected_trackers
-  def initialize torrent, options = {:tracker_timeout => 10,
+  def initialize torrent, options = {:tracker_timeout => 10, # Default is 10, maybe too big?
                                      :use_announce_list_on_initial_connection => true,
                                      :port => 6881}
     @torrent_file = torrent
     @options = options
+    
+    # Trackers we were able to connect to successfully.
     @connected_trackers = []
+    
+    # Trackers that timed out or refused connection.
     @disconnected_trackers = []
     
     # Set up .torrent file information for GET requests
     info = @torrent_file.to_h['info'].bencode
+    
+    # This is what will be passed for the info_hash GET parameter.
     @info_hash = URI.encode Digest::SHA1.digest(info).force_encoding('binary')
+    
+    # This is what will be passed for the peer_id GET parameter.
     @peer_id = generate_peer_id
     
-    # Set up hash of tracker for the torrent (from announce and announce-list).
+    # Set up hash of tracker info for the torrent (from announce and announce-list).
     @trackers = []
     torrent_hash = @torrent_file.to_h
     announce_uri = URI.parse torrent_hash['announce']
     @trackers << {:url => announce_uri.to_s, :scheme => announce_uri.scheme,
                   :port => announce_uri.port, :path => announce_uri.path,
                   :host => announce_uri.host}
+                  
+    # Honor the parameter from @options. As far as the parameter itself goes,
+    # there is no particular reason for having it, but this class will connect
+    # to every tracker it can find otherwise. However, if you set this to false,
+    # the constructor will only connect to the tracker that is the value for the
+    # "announce" key in the metainfo file dictionary, and you can still connect to
+    # the other trackers with the establish_connections method.
+    # Note: The connection to the tracker at "announce" may fail, so at some point
+    # establish_connection ought to be called.
     if @options[:use_announce_list_on_initial_connection]
       torrent_hash['announce-list'].each do |list|
         list.each do |tracker|
@@ -44,6 +62,7 @@ class TrackerHandler
     establish_connections
   end
   
+  # Establish connections to the trackers defined in "announce-list".
   def establish_connections
     for tracker in @trackers
       # TODO handle UDP trackers.
@@ -53,18 +72,20 @@ class TrackerHandler
           @connected_trackers << {:tracker => tracker,
             :connection => Net::HTTP.start(tracker[:host], tracker[:port])}
         end
-      rescue Errno::ECONNREFUSED => error
-        @disconnected_trackers << {:tracker => tracker, :error => error}
-      rescue Timeout::Error => error
+      # Connection refused or timed out or whatever.
+      rescue => error
         @disconnected_trackers << {:tracker => tracker, :error => error}
       end
     end
   end
   
+  # Make http request to the tracker and get results.
   def request params = {}
+    # All parameters defined in the specification are required except for ip, numwant,
+    # key and trackerid.
     required_params = [:uploaded, :downloaded, :left, :compact, :no_peer_id, :event, :index]
     diff = required_params - params.keys
-    if diff == []
+    if diff.empty?
       request_string = "#{tracker[:path]}?"                 +
                        "info_hash=#{@info_hash}&"           +
                        "peer_id=#{@peer_id}&"               +
@@ -82,39 +103,14 @@ class TrackerHandler
     else
       raise ArgumentError, "Required values for keys: #{diff.to_s} not provided"
     end
-    tracker_request = Net::HTTP::Get.new request_string
-    @connected_trackers[:connection].request tracker_request
+    # Make, and return the body of, the request.
+    @connected_trackers[:connection].request(Net::HTTP::Get.new(request_string)).body
   end
   
 private
+  # Generate a peer_id for the GET request. It can be arbitrary but has to be 20 bytes
+  # and url-encode.
   def generate_peer_id
     URI.encode Digest::SHA1.digest(Time.now.hash.to_s).force_encoding('binary')
   end
 end
-
-
-t = TorrentFile.open '/home/mikhail/Downloads/Source_Code_(2011)_DVDRip_XviD-MAX.6525961.TPB.torrent'
-
-s = TrackerHandler.new t
-
-puts s.connected_trackers.inspect
-puts s.disconnected_trackers.inspect
-
-=begin
-tracker_url = URI.parse t.to_h['announce-list'][0][0]
-
-tracker_host = tracker_url.host
-tracker_port = tracker_url.port
-tracker_path = tracker_url.path
-
-info = t.to_h['info'].bencode
-info_hash = URI.encode Digest::SHA1.digest(info).force_encoding('binary')
-
-puts tracker_host, tracker_port
-http = Net::HTTP.start tracker_host, tracker_port
-
-request = Net::HTTP::Get.new "#{tracker_path}?info_hash=#{info_hash}&peer_id=#{info_hash}&port=42309&uploaded=1&downloaded=5&left=&compact=1&event=started"
-r = http.request request
-
-puts r.body
-=end
